870c00aeae367245e62cd68222e1ceec1f795c96
Harring <harring_@hotmail.com>
2023-12-07 12:08:11 +0100

    Update setting file with db location and deployment file to save db state

--- FruitScan/FruitScan/settings.py
+++ FruitScan/FruitScan/settings.py
@@ -78,7 +78,7 @@ MODEL_VERSION = 1
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '/data/db.sqlite3'),
     }
 }
 
