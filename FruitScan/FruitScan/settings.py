25b57a497b0beacabf19794d1fa198e17e75a148
Harring <harring_@hotmail.com>
2023-12-08 13:44:21 +0100

    Update media folder path and deployment to include persistent media storage

--- FruitScan/FruitScan/settings.py
+++ FruitScan/FruitScan/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 # Base url to serve media files
 MEDIA_URL = "/media/"
 # Path to where the media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "/media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
